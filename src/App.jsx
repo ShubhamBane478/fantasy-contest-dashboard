d0addf15b8c0e1cbbef9c47077a8155c889ddeb9
Shubham Bane <shubhambane435@gmil.com>
2024-06-15 11:18:05 +0530

    Basic dashborad Layout completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-
 import './App.css'
+
+import {Dashboard} from './pages/Dashboard'
 
 function App() {
   
 
   return (
     <>
-     <h1 className="text-4xl font-bold underline text-green-300">
-      Hello world!
-    </h1>
+     <Dashboard/>
     </>
   )
 }
